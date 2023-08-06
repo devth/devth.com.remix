b5a1e896ed726791b54437c26d60cd58f091498b
Trevor Hartman <trevorhartman@gmail.com>
2023-08-06 14:12:00 -0600

    Setup Framer

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,18 @@
+import { Button, Container } from "@mui/material";
 import {
   Links,
   LiveReload,
   Meta,
-  Outlet,
   Scripts,
   ScrollRestoration,
+  useLocation,
+  useOutlet,
 } from "@remix-run/react";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function App() {
+  const outlet = useOutlet();
+
   return (
     <html lang="en">
       <head>
@@ -17,7 +22,20 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <AnimatePresence mode="wait" initial={false}>
+          <motion.main
+            key={useLocation().pathname}
+            initial={{ y: "+20%", opacity: 0 }}
+            animate={{ y: "0", opacity: 1 }}
+            exit={{ opacity: 0, y: "-20%" }}
+            transition={{ duration: 0.1 }}
+          >
+            <Container>{outlet}</Container>
+            <Button variant="contained" color="primary">
+              Hello
+            </Button>
+          </motion.main>
+        </AnimatePresence>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
